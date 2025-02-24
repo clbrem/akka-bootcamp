3315081f9258633b9211bae0e43c0f03f544ead5
Chris Bremer <cbremer4@gmail.com>
2025-02-23 21:42:46 -0600

    Updated Word Counter

--- unit-1/completed/AkkaWordCounter/Counter.fs
+++ unit-1/completed/AkkaWordCounter/Counter.fs
@@ -1,6 +1,8 @@
 namespace AkkaWordCounter
+open Akka.Event
 open Akka.FSharp
 open Akka.Actor
+open System
 
 type CounterCommand =
     | CountTokens of string list
@@ -8,38 +10,55 @@ type CounterCommand =
     | FetchCounts of IActorRef
 type DocumentCommand =
     | ProcessDocument of string
-
-    
-
 type Counter = Map<string, int>
     
-module Counter =
+module Counter =    
     let empty = Map.empty
     let add token counter =
         if Map.containsKey token counter then
             Map.add token ( 1 + Map.find token counter )  counter
         else
             Map.add token 1 counter
-    let counter =
+    let counterActor =
         fun (mailbox: Actor<CounterCommand>) ->
-            let rec loop subscribers doneCounting counts = actor {
-                let! message = mailbox.Receive()
-                match message with
-                | CountTokens tokens ->
-                    let newCounts =
-                        List.foldBack add tokens counts
-                    return! loop subscribers doneCounting newCounts
-                | ExpectNoMoreTokens ->
-                    
-                    
-                    return! loop Set.empty true counts
-                | FetchCounts requester ->
-                    if doneCounting then
-                        requester.Tell(counts)
-                    else
-                        return! loop (Set.add requester subscribers) doneCounting counts
-            }
-            loop Set.empty false empty 
-        
-    
-
+            let rec loop subscribers doneCounting counts =
+                actor {
+                    let! message = mailbox.Receive()
+                    match message with
+                    | CountTokens tokens ->
+                        let newCounts =
+                            List.foldBack add tokens counts                        
+                        return! loop subscribers doneCounting newCounts
+                    | ExpectNoMoreTokens ->
+                        mailbox.Log.Value.Info(
+                            "Completed counting tokens - found [{0}] unique tokens",
+                            Map.count counts
+                            )
+                        for subscriber: IActorRef in subscribers do
+                            subscriber.Tell(counts)
+                        return! loop Set.empty true counts
+                    | FetchCounts ref when doneCounting ->                            
+                            ref.Tell(counts)
+                            return! loop subscribers doneCounting counts
+                    | FetchCounts ref ->
+                            return! loop (Set.add ref subscribers) doneCounting counts
+                }
+            loop Set.empty false empty
+    let private TOKEN_BATCH_SIZE = 10;
+    let parserActor (counter: IActorRef)=
+        fun (mailbox: Actor<DocumentCommand>) ->
+            let rec loop () = 
+                actor {
+                    match! mailbox.Receive() with
+                    | ProcessDocument str ->                        
+                        str.Split(" ")
+                        |> List.ofArray
+                        |> List.filter (String.IsNullOrWhiteSpace >> not) 
+                        |> List.chunkBySize TOKEN_BATCH_SIZE
+                        |> List.iter (CountTokens >> counter.Tell)
+                        counter.Tell(ExpectNoMoreTokens)
+                    return! loop ()
+                }
+            loop ()
+            
+            
--- unit-1/completed/AkkaWordCounter/Program.fs
+++ unit-1/completed/AkkaWordCounter/Program.fs
@@ -1,38 +1,36 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
+open System.Threading
 open Akka
 open System
 open Akka.Event
 open Akka.FSharp
 open Akka.Actor
 open System.Threading.Tasks
+open AkkaWordCounter
 
 [<EntryPoint>]
-let main args = 
-    use system = System.create "my-system" (Configuration.load())
-    let sender =
-        actorOf2 (
-            fun actor message ->
-                system.Log.Info $"Received: {message}"
-                system.Log.Info $"I'm {actor.Self.Path}"
-                system.Log.Info $"Sender: {actor.Context.Sender.Path}"
+let main args =
+    let thread = 
+        task {
+            use system = System.create "my-system" (Configuration.load())
+            let counter = spawn system "counter" Counter.counterActor
+            let parser = spawn system "parser" (Counter.parserActor counter)
+            
+            parser.Tell(
+                """
+                It was the best of times it was the worst of times.
+                In fair Verona where we lay our scene.
+                Happy families are all alike;
+                every unhappy family is unhappy in its own way.
+                """ |> ProcessDocument
                 )
-        |> spawn system "sender-actor" 
-    let aref =        
-        spawn system "my-actor"
-            (fun mailbox ->
-                let rec loop() = actor {
-                    let! message = mailbox.Receive()                    
-                    system.Log.Info($"Received: {message}")                    
-                    mailbox.Context.Sender.Tell("got it!")
-                    // handle an incoming message
-                    return! loop()
-                }
-                loop()
-            )
-    system.Log.Info("hello")
-    aref.Tell("hello", sender)
-    aref.Tell("goodbye", sender)
-    system.Terminate().GetAwaiter().GetResult() 
-    0
+            let factory = System.Func<IActorRef, obj>(fun ref -> FetchCounts ref :> obj)
+            let! promise = counter.Ask<Counter>(factory, TimeSpan.FromSeconds(1L), CancellationToken.None)
+            printfn $"%A{promise |> Map.filter (fun k v -> v > 1)}"
+//            let! resp = promise            
+            return 0
+        }
+    thread.GetAwaiter().GetResult()
+    
 
 
