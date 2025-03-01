c063202ef715a8425212dbebca73febf0ee9bed2
Chris Bremer <cbremer4@gmail.com>
2025-03-01 15:36:05 -0600

    tweaking timerinos

--- unit-1/completed/AkkaWordCounterZwei.Test/Timeout.Spec.fs
+++ unit-1/completed/AkkaWordCounterZwei.Test/Timeout.Spec.fs
@@ -3,7 +3,6 @@ open AkkaWordCounterZwei.Actors
 open Akka.Actor
 open Akka.FSharp
 
-open AkkaWordCounterZwei.Actors
 open Xunit
 open Akka.TestKit.Xunit2
 open Xunit.Abstractions
@@ -16,14 +15,14 @@ type private Testing =
 type Timeout_Spec(helper: ITestOutputHelper) as this =
     inherit TestKit(config="akka.loglevel=DEBUG",output=helper)    
     let timerName = "timer"
-    let timeout = System.TimeSpan.FromSeconds(5.0)
+    let timeout = System.TimeSpan.FromSeconds(2.0)
     let actorok = 
         fun (mailbox: Actor<Testing>) ->            
             let rec loop sender =
                 actor {
                     match! mailbox.Receive() with
                     | Ping ->
-                        mailbox.Context.OnTimeout(timeout, Pong, timerName)                        
+                        mailbox.Context.OnTimeout(Pong, timerName, timeout)                        
                         return! loop (mailbox.Sender()|> Some)
                     | Pong ->
                         match sender with
@@ -31,7 +30,36 @@ type Timeout_Spec(helper: ITestOutputHelper) as this =
                         | _ -> ()                        
                         return! loop None
                         }
-            loop None    
+            loop None
+    let bactorok =
+        fun (mailbox: Actor<Testing>) ->            
+            let rec loop (sender: IActorRef option) shouldPong =
+                if shouldPong then
+                    actor {
+                            match! mailbox.Receive() with
+                            | Ping ->
+                                mailbox.Context.OnTimeout(Pong, timerName, timeout)                        
+                                return! loop sender shouldPong 
+                            | Pong ->
+                                match sender with
+                                | Some s -> s.Tell "Done!"
+                                | _ -> ()                        
+                                return! loop None false
+                                }
+                else
+                    actor {
+                            match! mailbox.Receive() with
+                            | Ping ->
+                                mailbox.Context.OnTimeout(Ping, timerName, timeout)                        
+                                return! loop (mailbox.Sender()|> Some) true 
+                            | Pong ->
+                                match sender with
+                                | Some s -> s.Tell "Done!"
+                                | _ -> ()                        
+                                return! loop None false
+                                }
+                
+            loop None false
     [<Fact>]
     let ``Testing Timeout``() =
         task {
@@ -39,5 +67,12 @@ type Timeout_Spec(helper: ITestOutputHelper) as this =
             let! resp = actor.Ask<string>(Ping)
             Assert.Equal("Done!", resp)
         }
+    [<Fact>]
+    let ``Testing double timeout 2``() =
+        task {
+            let actor = spawn this.Sys "test" bactorok
+            let! resp = actor.Ask<string>(Ping)
+            Assert.Equal("Done!", resp)
+        }        
 
 
